a283f97824c0f0c88640e6a97e2d19871c168b50
JLJL <juanluisjaramillolopez@gmail.com>
2024-01-11 14:12:28 -0600

    Se crea la funcion de eliminar tareas de la base de datos

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import UserContext from "../context/UserContext";
-import { getDocs, collection } from "firebase/firestore"; //para traerla coleccion de datos de firestore
+import { getDocs, collection, onSnapshot, doc, deleteDoc } from "firebase/firestore"; //para traerla coleccion de datos de firestore
 
 //?Traemos la funcion crada en UseSttate que trae los datos de firestore
 const TaskList = () => {
@@ -11,24 +11,35 @@ const TaskList = () => {
 
   //*creamos una funcion asyncronica para llamar los datos
   useEffect(() => {
-    const getTaskList = async () => {
+    //*onSnapshot nos ayuda a acutualizar de manera asincrona los datos para evitar refrescar la pagina
+    onSnapshot(collection(store, "taskStore"), ()=> {
+      const getTaskList = async () => {
       const querySnapshot = await getDocs(collection(store, "taskStore"));
-      const dataQuerySnapshot = querySnapshot.docs.map((item) => item.data());
+      const dataQuerySnapshot = querySnapshot.docs.map((item) => ({ id: item.id, ...item.data()}));  
+      //?Guardamos como un objeto el id y la data que se guarda en la base de datos
       setdata(dataQuerySnapshot);
       console.log(dataQuerySnapshot);
     };
     getTaskList();
+    });
+    
   }, [store]);
+    //TODO: en esta funcion bamos a borrar loos datos de una tarea del storage
+    const deleteTask = async (id) => {
+      console.log("task.id:", id);
+      await deleteDoc(doc(store, 'taskStore', id));
+    }
+  
 
   return (
     <div className="m-5">
       {data.map((task) => (
-        <div className="card" key={task}>
+        <div className="card" key={task.id}>
           <div className="card-body">
             <h5 className="card-title text-center">{task.task}</h5>
             <p className="card-text">{task.description}</p>
             <div className="d-flex justify-content-center align-items-center">
-              <button className="btn btn-danger border m-1">Delete Task</button>
+              <button className="btn btn-danger border m-1" onClick={()=> deleteTask(task.id)}>Delete Task</button>
               <button className="btn btn-light border m-1">Edit Task</button>
             </div>
           </div>
